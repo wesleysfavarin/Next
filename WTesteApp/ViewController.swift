0776f630d743ea752901f8b0f68978b3c2a70b27
mac <mac@macbook-pro.local>
2019-03-10 17:04:11 -0300

    Initial Commit

---
+++ WTesteApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WTesteApp
+//
+//  Created by mac on 10/03/19.
+//  Copyright © 2019 Wesley S. Favarin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
